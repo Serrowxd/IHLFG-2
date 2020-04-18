14c8f0c935213ba0d797d1b940612f9a6d2de69d
Kevin Jolley <serrowpls@gmail.com>
2020-04-18 07:58:11 -0500

    router added, reset, initial dashboard

---
+++ src/components/dashboard.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function Dashboard(props) {
+  const [user, setUser] = useState({});
+
+  return (
+    <div>
+      <h1> Henlo (OvO") </h1>
+    </div>
+  );
+}
+
+export default Dashboard;
